cddab7354c1fb7220295992d4e8263d81da79571
peter-fogg <peter.p.fogg@gmail.com>
2014-11-17 11:05:59 -0500

    a10

---
+++ a10.rkt
@@ -0,0 +1,45 @@
+#lang racket
+(require C311/mk)
+(require C311/numbers)
+(provide (all-defined-out) (all-from-out C311/mk) (all-from-out C311/numbers))
+
+;; Part I Write the answers to the following problems using your
+;; knowledge of miniKanren.  For each problem, explain how miniKanren
+;; arrived at the answer.  You will be graded on the quality of your
+;; explanation; a full explanation will require several sentences.
+
+;; 1 What is the value of 
+(run 2 (q)
+  (== 5 q)
+  (conde
+    [(conde [(== 5 q)
+	     (== 6 q)])
+     (== 5 q)]
+    [(== q 5)]))
+;;
+;;
+;;
+;;
+;;
+
+;; 2 What is the value of
+(run 1 (q) 
+  (fresh (a b) 
+    (== `(,a ,b) q)
+    (absento 'tag q)
+    (symbolo a)))
+;;
+;;
+;;
+;;
+;;
+
+;; 3 What do the following miniKanren constraints mean?
+;; a not-pairo
+;; b =/=
+;; c absento
+;; d numbero
+;; e symbolo
+
+;; Part II goes here.
+
